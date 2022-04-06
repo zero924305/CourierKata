d2d6ed9a6ed69a1cb106eb6bdad3638a11e0adfe
Altino <zero924305@gmail.com>
2022-04-06 02:27:02 +0100

    create the GetParcelSize() to select the parcel size details base on the input

--- Courier Kata Test/ParcelTest.cs
+++ Courier Kata Test/ParcelTest.cs
@@ -7,13 +7,13 @@ namespace Courier_Kata_Test
     public class ParcelTest
     {
         [Theory]
-        [InlineData(1, 0, 1, 1)]
-        [InlineData(1, 1, 0, 1)]
-        [InlineData(1, 1, 1, 0)]
-        [InlineData(0, 1, 1, 1)]
-        public void CheckParcelConstructorsThrowInvalidError(int len, int wid, int hei, int wei)
+        [InlineData(1, 0, 1, 1,false)]
+        [InlineData(1, 1, 0, 1, false)]
+        [InlineData(1, 1, 1, 0, false)]
+        [InlineData(0, 1, 1, 1, false)]
+        public void CheckParcelConstructorsThrowInvalidError(int len, int wid, int hei, int wei,bool isHeavy)
         {
-            Assert.Throws<InvalidOperationException>(() => { new Parcel(len, wid, hei, wei); });
+            Assert.Throws<InvalidOperationException>(() => { new Parcel(len, wid, hei, wei, isHeavy); });
         }
     }
 }
---
+++ Courier Kata/Model/ParcelInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courier_Kata.Model
+{
+    //ParcelInfo store ParcelSize Details
+    public class ParcelInfo
+    {
+        public string ParcelSizeName { get; set; }
+        public int ParcelSizedimensions_mm { get; set; }
+        public int ParcelWeightLimit_g { get; set; }
+        public int ParcelSizeFee { get; set; }
+        public int ParcelSizeOverWeightPenaltyFeePerKg { get; set; }
+
+        public ParcelInfo(string ParcelSizeName, int ParcelSizedimensions_mm, int ParcelWeightLimit_g, int ParcelSizeFee,int ParcelSizeOverWeightPenaltyFeePerKg)
+        {
+            this.ParcelSizeName = ParcelSizeName;
+            this.ParcelSizedimensions_mm = ParcelSizedimensions_mm;
+            this.ParcelWeightLimit_g = ParcelWeightLimit_g;
+            this.ParcelSizeFee = ParcelSizeFee;
+            this.ParcelSizeOverWeightPenaltyFeePerKg = ParcelSizeOverWeightPenaltyFeePerKg;
+        }
+    }
+}
