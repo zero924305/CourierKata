218fced53014a1485a5fde7f97f6c9b78621e4f5
Altino <zero924305@gmail.com>
2022-04-06 03:23:56 +0100

    Create a enum for storing the Parcel Discount Type

--- Courier Kata/Model/Order.cs
+++ Courier Kata/Model/Order.cs
@@ -5,12 +5,26 @@ using System.Threading.Tasks;
 
 namespace Courier_Kata.Model
 {
+    public enum ParcelDiscountType { 
+        None = 0,
+        //Every 4 small parcel can get the discount
+        Small = 4,
+        //Every 3 Medium parcel can get the discount
+        Medium = 3,
+        //In mixed order, every 5 parcel can get the discount
+        Mixed = 5
+    }
+
     public class Order
     {
         public List<Parcel> ParcelDetails { get; set; }
+
         //using Linq to get the total number of parcel in a order
         public int TotalParcelInOrder => ParcelDetails.Count;
 
+        //Check does this order need for fast shipping 
+        public bool IsSpeedyOrder { get; set; }
 
+        
     }
 }
