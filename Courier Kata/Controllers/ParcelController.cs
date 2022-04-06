2f56ab86c308edf166ab0e171add98983ad61dab
Altino <zero924305@gmail.com>
2022-04-06 01:41:18 +0100

    Create a Parcel API Controller

---
+++ Courier Kata/Controllers/ParcelController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Courier_Kata.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParcelController : ControllerBase
+    {
+        // GET: api/<ParcelController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+
+
+    }
+}
