71b4a12b349f35d52c1a45d40c60eaf3a797aeb8
Altino <zero924305@gmail.com>
2022-04-06 03:19:53 +0100

    create a order class filter parcel detail

---
+++ Courier Kata/Model/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courier_Kata.Model
+{
+    public class Order
+    {
+        public List<Parcel> ParcelDetails { get; set; }
+        //using Linq to get the total number of parcel in a order
+        public int TotalParcelInOrder => ParcelDetails.Count;
+
+
+    }
+}
