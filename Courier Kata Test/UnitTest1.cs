59aab41dd1601cbf11d1aa254e3b6e142d3d2cdc
Altino <zero924305@gmail.com>
2022-04-06 01:58:56 +0100

    1. Create the PacelSize Enum with dimensions limit,  Parcel class and Parcel constructors
    2. Create a Test project for "courier kata" using xUnite testing  framework

---
+++ Courier Kata Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Courier_Kata.Model;
+
+namespace Courier_Kata_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Courier Kata/Model/Parcel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Courier_Kata.Model
+{
+    //Parcel Size Details
+    public enum ParcelSize
+    { 
+        Small = 10,
+        Medium = 50, 
+        Large = 100,
+        XL
+    }
+
+
+    public class Parcel
+    {
+        public Parcel(int len, int wid, int hei,int wei)
+        {
+            if (len <= 0 || wid <= 0 || hei <= 0 || wei <= 0)
+                throw new InvalidOperationException("The package dimensions or weight are invalid");
+
+            this.Length_mm = len;
+            this.Width_mm = wid;
+            this.Height_mm = hei;
+            this.Weight_g = wei;
+        }
+
+        //set the length in millimetre 
+        public int Length_mm { get; set; }
+
+        //set the Width in millimetre 
+        public int Width_mm { get; set; }
+
+        //set the Height in millimetre 
+        public int Height_mm { get; set; }
+
+        //set the Weight in gram
+        public int Weight_g { get; set; }
+
+    }
+}
